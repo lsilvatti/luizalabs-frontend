9cab7e78d6721100623db7f84ae1fa3c3356a437
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-03 02:00:57 -0300

    feat: grid component

---
+++ src/components/home/CardGrid/CardGrid.tsx
@@ -0,0 +1,84 @@
+import { useGetCharacters } from "hooks";
+import CharacterCard from "../CharacterCard/CharacterCard";
+import CardGridOptions from "./CardGridOptions";
+import "./CardGrid.scss";
+import { useEffect, useState } from "react";
+import { Character, orderBy } from "components/types";
+import { Loading } from "components/common";
+import CardGridPagination from "./CardGridPagination";
+import { getFavoriteList } from "utils";
+import face_sad from "assets/svg/face-frown-open-solid.svg";
+
+export default function CardGrid() {
+  const [alphabeticOrder, setAlphabeticOrder] = useState<orderBy>("name");
+  const [page, setPage] = useState(0);
+  const [favoriteOnly, setFavoriteOnly] = useState(false);
+  const [heroList, setHeroList] = useState<Character[]>();
+  const [count, setCount] = useState(0);
+  const { data, loading, error } = useGetCharacters("", alphabeticOrder, page);
+
+  useEffect(() => {
+    const storageFavorite = getFavoriteList();
+    if (favoriteOnly && storageFavorite) {
+      console.log(favoriteOnly);
+      setHeroList(storageFavorite);
+      setCount(storageFavorite.length);
+      return;
+    }
+
+    if (data) {
+      console.log(data);
+      setCount(data.total);
+      setHeroList(data.results);
+    }
+    if (loading) setCount(0);
+  }, [data, loading, favoriteOnly, page]);
+
+  return (
+    <section className="card-grid">
+      <div className="card-grid__header">
+        <span>
+          {loading
+            ? "Carregando listagem"
+            : count
+            ? `Foram encontrados ${count} heróis`
+            : "Não foram encontrados heróis"}
+        </span>
+        <CardGridOptions
+          setAlphabetic={setAlphabeticOrder}
+          setFavoritesOnly={setFavoriteOnly}
+          loading={loading}
+        ></CardGridOptions>
+      </div>
+      {heroList && !loading && data ? (
+        <section className="card-grid__main">
+          <div className="card-grid__grid">
+            {heroList.map((character, index, array) => {
+              return (
+                <CharacterCard
+                  key={character.id}
+                  character={character}
+                ></CharacterCard>
+              );
+            })}
+          </div>
+          {count > 20 && (
+            <CardGridPagination
+              setActivePage={setPage}
+              count={count}
+              activePage={page}
+            ></CardGridPagination>
+          )}
+          {!count && (
+            <div className="card-grid__no-results">
+              <img src={face_sad} alt="" />
+              Não há resultados para mostrar!
+            </div>
+          )}
+        </section>
+      ) : (
+        <Loading></Loading>
+      )}
+    </section>
+  );
+}
