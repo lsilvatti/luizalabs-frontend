3a1c1ba4986b1bb7da039e71c9cc6e5908156786
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-03 01:59:32 -0300

    feat :add order, page and tweaks on useGetCharacters

--- src/hooks/useGetCharacters.ts
+++ src/hooks/useGetCharacters.ts
@@ -1,41 +1,43 @@
-import { useState, useEffect } from 'react';
-import { AxiosError } from 'axios';
-import { getHash } from 'utils/getHash';
-import { marvelService } from 'services';
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import { getHash } from "utils/getHash";
+import { marvelService } from "services";
+import {
+  CharacterDataContainer,
+  HeroesFetchable,
+  orderBy,
+} from "components/types";
 
-interface HeroesFetchable {
-    ts: number;
-    hash: string;
-    nameStartsWith?: string;
-}
+const useGetCharacters = (name?: string, order?: orderBy, page: number = 0) => {
+  const [data, setData] = useState<CharacterDataContainer>();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-const useGetCharacters = (name: string) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const requestParams: HeroesFetchable = {
+      ...getHash(),
+      offset: page * 20,
+      limit: 20,
+    };
+    if (name) requestParams.nameStartsWith = name;
+    if (order) requestParams.orderBy = order;
+    (async () => {
+      setLoading(true);
+      try {
+        const response = await marvelService.get("v1/public/characters", {
+          params: requestParams,
+        });
+        if (response.data.data) setData(response.data.data);
+      } catch (err) {
+        const error = err as Error | AxiosError;
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [name, order, page]);
 
-    useEffect(() => {
-        const requestParams: HeroesFetchable = {
-            ...getHash()
-        };
-        if(name) requestParams.nameStartsWith = name;
-        
-        (async () => {
-            try {
-                const response = await marvelService.get('v1/public/characters', {
-                    params: requestParams
-                });
-                setData(response.data.data);
-            } catch(err) {
-                const error = err as Error | AxiosError;
-                setError(error.message)
-            } finally {
-                setLoading(false)
-            }
-        })();
-    }, [name])
-
-    return {data, error, loading}
-}
+  return { data, error, loading };
+};
 
 export default useGetCharacters;
