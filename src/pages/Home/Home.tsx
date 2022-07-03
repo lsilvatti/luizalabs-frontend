2ac4c43340c1c2b202c6d6e1017620e17820058b
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-03 03:09:43 -0300

    feat: home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import logo from "assets/svg/logo.svg";
+import { SearchBar } from "components/common";
+import CardGrid from "components/home/CardGrid/CardGrid";
+
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <main className="luiza-heroes-home">
+      <div className={"luiza-heroes-home__header container"}>
+        <img src={logo} />
+        <h1>Explore o Universo</h1>
+        <p>
+          Mergulhe no domínio deslumbrante de todos os personagens clássicos que
+          você ama - e aqueles que você descobrirá em breve
+        </p>
+        <SearchBar placeholder="Procure por heróis"></SearchBar>
+      </div>
+
+      <CardGrid></CardGrid>
+    </main>
+  );
+}
