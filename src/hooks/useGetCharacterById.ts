bcba59520f1b5e4b6b19bee71830d7055b4419d4
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-04 11:57:13 -0300

    refactor: usegetcharacterbyid ifs and refactor code

--- src/hooks/useGetCharacterById.ts
+++ src/hooks/useGetCharacterById.ts
@@ -1,32 +1,35 @@
-import { useState, useEffect } from 'react';
-import { AxiosError } from 'axios';
-import { getHash } from 'utils/getHash';
-import { marvelService } from 'services';
-
-
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import { getHash } from "utils/getHash";
+import { marvelService } from "services";
+import { Character } from "components/types";
 
 const useGetCharacterById = (id: number) => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<Character>();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => { 
-        (async () => {
-            try {
-                const response = await marvelService.get(`v1/public/characters/${id}`, {
-                    params: getHash()
-                });
-                setData(response.data.data);
-            } catch(err) {
-                const error = err as Error | AxiosError;
-                setError(error.message)
-            } finally {
-                setLoading(false)
-            }
-        })();
-    }, [id])
+  useEffect(() => {
+    if (id === 0) return;
+    setLoading(true);
+    (async () => {
+      try {
+        const response = await marvelService.get(`v1/public/characters/${id}`, {
+          params: getHash(),
+        });
+        const characterDataContainer = response.data.data;
+        if (characterDataContainer && characterDataContainer.count === 1)
+          setData(characterDataContainer.results[0]);
+      } catch (err) {
+        const error = err as Error | AxiosError;
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [id]);
 
-    return {data, error, loading}
-}
+  return { data, error, loading };
+};
 
 export default useGetCharacterById;
