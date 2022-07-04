6e1122be22391933e0a3c8cee5d4f57db5cfdff5
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-04 11:56:49 -0300

    feat: getComics hook

---
+++ src/hooks/useGetComics.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import { getHash } from "utils/getHash";
+import { marvelService } from "services";
+import { ComicDataContainer } from "components/types";
+
+const useGetComics = (id: number) => {
+  const [data, setData] = useState<ComicDataContainer>();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (id === 0) return;
+    setLoading(true);
+    (async () => {
+      try {
+        const response = await marvelService.get(
+          `v1/public/characters/${id}/comics`,
+          {
+            params: getHash(),
+          }
+        );
+        if (response.data.data) setData(response.data.data);
+      } catch (err) {
+        const error = err as Error | AxiosError;
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [id]);
+
+  return { data, error, loading };
+};
+
+export default useGetComics;
