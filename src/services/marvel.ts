22cc135796ee4ac92a96444dcd1748e252560069
Leonardo Silvatti <leosilvatti0@gmail.com>
2022-07-05 19:23:27 -0300

    refactor: write prettier

--- src/services/marvel.ts
+++ src/services/marvel.ts
@@ -1,11 +1,11 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
-    const marvelService = axios.create({
-        baseURL: process.env.REACT_APP_MARVEL_BASE_URL
-    })
+const marvelService = axios.create({
+  baseURL: process.env.REACT_APP_MARVEL_BASE_URL,
+});
 
-    marvelService.defaults.params = {
-        apikey: process.env.REACT_APP_MARVEL_PUBLIC_API_KEY
-    }
+marvelService.defaults.params = {
+  apikey: process.env.REACT_APP_MARVEL_PUBLIC_API_KEY,
+};
 
 export default marvelService;
